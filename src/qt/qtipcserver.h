e96f2874b81e56914cde8736d6197198ac830c2a
Termocoin <ned91@libero.it>
2014-09-19 22:28:10 +0200

    first commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define bitxcoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "XCashURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
